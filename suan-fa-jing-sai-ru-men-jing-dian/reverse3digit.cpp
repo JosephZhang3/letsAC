715d92f1eef7f20ca92c2b3d4f1df0cec2f7171c
ZebulonFeng <jianghaozhang@qq.com>
2020-06-28 19:38:08 +0800

    rush

---
+++ suan-fa-jing-sai-ru-men-jing-dian/reverse3digit.cpp
@@ -0,0 +1,35 @@
+//
+// Created by zhangjianghao on 6/27/2020.
+//
+#include <sstream>
+#include "iostream"
+using namespace std;
+
+/**
+ * 三位数翻转
+ * 思路：百位数字等于n模100，十位数字等于n除10再模10（除10去掉个位，模10得到十位），个位数字等于n模10
+ *
+ * simple test case
+ * 127
+ * 100
+ * 999
+ *
+ * @return
+ */
+int main(){
+    int t;
+    string str;
+    getline(cin, str);
+    stringstream ss(str);// 流式处理，以空格分隔字符串
+    ss >> t;
+
+    // integer formal
+    int tar;
+    tar = t / 100 + t / 10 % 10 * 10 + t % 10 * 100;
+    cout << tar << endl;
+
+    // string formal
+    printf("%d%d%d", t%10, t/10%10, t/100);
+
+    return 0;
+}
