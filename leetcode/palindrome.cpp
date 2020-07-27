4afea3ca0a0f59cdd0ccf4c3e2dd8b7e43023551
jianghao.zhang <jianghaozhang@qq.com>
2020-07-27 11:49:23 +0800

    once

---
+++ leetcode/palindrome.cpp
@@ -0,0 +1,20 @@
+//
+// Created by mac on 2020/6/28.
+//
+#include "iostream"
+using namespace std;
+
+// 判断回文数字，要求不转换成字符串
+
+bool isPalindrome(int x){
+    bool rt = true;
+}
+
+int main(){
+    cout << isPalindrome(121);
+    cout << isPalindrome(-121);
+    cout << isPalindrome(4567);
+    cout << isPalindrome(-4567);
+    cout << isPalindrome(1111);
+    cout << isPalindrome(0);
+}
