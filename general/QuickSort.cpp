38236bf0decc626bdb0ce6543fff468b608f3e3a
jianghao.zhang <jianghaozhang@qq.com>
2020-11-09 15:44:49 +0800

    rename

--- general/QuickSort.cpp
+++ general/QuickSort.cpp
@@ -14,25 +14,31 @@ int n;
 void quick_sort(int q[], int l, int r) {
     for (int i = 0; i < n; i++) printf("%d ", q[i]);
     cout << endl;
-    // 当左右标记重合时，停止
-    if (l >= r) return;
+
+    // 区间仅包含一个元素时，停止递归
+    if (l >= r) {
+        cout << "l = " << l << ", r = " << r << " 时终止" << endl;
+        return;
+    }
 
     int i = l - 1, j = r + 1, x = q[(l + r) >> 1];
-    cout << "i = " << i << ", j = " << j << ", 轴心点元素index = " << ((l + r) >> 1) << ", value = " << x << endl << endl;
+    cout << "i = " << i << ", j = " << j << ", 轴心点元素index = " << ((l + r) >> 1) << ", value = " << x << endl;
 
     while (i < j) {
-        do i++; while (q[i] < x);// 左区间遇到大于等于x的元素时，停下
-        do j--; while (q[j] > x);// 右区间遇到小于等于x的元素时，停下
-        // 当i和j未交错时，交换两元素
+        do i++; while (q[i] < x);// i指针右移，遇到大于等于x的元素时，停下
+        do j--; while (q[j] > x);// j指针左移，遇到小于等于x的元素时，停下
+        // 当i和j指针未相交或错过时，交换两指针位置的值。使得较大元素移到轴心点元素右侧，较小元素移动到轴心点左侧
         if (i < j) {
             swap(q[i], q[j]);
             cout << "交换了索引 " << i << "和" << j << endl;
+        } else{
+            cout << endl;
         }
     }
 
-    // 轴心点左区间
+    // 轴心点左区间递归
     quick_sort(q, l, j);
-    // 轴心点右区间
+    // 轴心点右区间递归
     quick_sort(q, j + 1, r);
 }
 
@@ -45,7 +51,7 @@ int main() {
     for (int i = 0; i < n; i++) scanf("%d", &q[i]);
     quick_sort(q, 0, n - 1);
     // 不使用 & 则是直接取索引位置i的地址
-    cout << "输出数组";
+    cout << "输出数组 ";
     for (int i = 0; i < n; i++) printf("%d ", q[i]);
     return 0;
 }
